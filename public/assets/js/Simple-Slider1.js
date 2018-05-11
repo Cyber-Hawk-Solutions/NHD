03d49258e4e2547eba0686e224f6e59fb8e6de21
James Canice <cajames@abiit.internal>
2018-05-11 15:25:29 -0400

    change slider controller and add effect

--- public/assets/js/Simple-Slider1.js
+++ public/assets/js/Simple-Slider1.js
@@ -1,14 +1,20 @@
 $(function(){
 
-    // Initializing the swiper plugin for the slider.
-    // Read more here: http://idangero.us/swiper/api/
-    
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev'
-    });
+  // Initializing the swiper plugin for the slider.
+  // Read more here: http://idangero.us/swiper/api/
+  
+  var mySwiper = new Swiper ('.swiper-container', {
+    autoplay: 4000,
+    loop: true,
+    disableOnInteraction: false,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+  });
     
 });
